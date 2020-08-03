c38adc0f8f55bf9ffbc01a724bfb0a60c5cd83cf
oliviasmith311 <olivia.smith.31196@gmail.com>
2020-08-03 15:56:29 -0400

    all posts and all hashtags displaying

--- src/main/frontend/js/app.js
+++ src/main/frontend/js/app.js
@@ -2,19 +2,88 @@ fetch('http://localhost:8080/')
         .then(response => response.json())
         .then(response => displayAllPosts(response));
 
+const anchor = document.querySelector('.anchor');
+
 const displayAllPosts = (jsonResponse) => {
-    const anchor = document.querySelector('.anchor');
+    anchor.classList.add('anchor');
+    const postHeader = document.createElement('div');
+    postHeader.innerText = "All Posts";
+    postHeader.classList.add('postHeader');
+    anchor.appendChild(postHeader);
+
     jsonResponse.forEach(post => {
+
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post');
+
         let postTitle = document.createElement('div');
         postTitle.innerText = post.title;
-        anchor.appendChild(postTitle);
+        postDiv.appendChild(postTitle);
         postTitle.classList.add('title');
+
+        let postAuthor = document.createElement('div');
+        postAuthor.innerText = "Author: " + post.author.name;
+        postDiv.appendChild(postAuthor);
+        postAuthor.classList.add('author');
+
+        let postBody = document.createElement('div');
+        postBody.innerText = post.postBody;
+        postDiv.appendChild(postBody);
+        postBody.classList.add('body');
+
+        const hashtagsAll = document.createElement('div');
+        hashtagsAll.classList.add('hashtagsAll');
 
         let hashtags = post.hashtags;
         hashtags.forEach(hashtag => {
             let postHashtags = document.createElement('div');
-            postHashtags.innerText = hashtag.content;
-            anchor.appendChild(postHashtags);
+            postHashtags.innerText = "#" + hashtag.content;
+            hashtagsAll.appendChild(postHashtags);
+            postHashtags.classList.add('hashtags');
         })
+        postDiv.appendChild(hashtagsAll);
+        anchor.appendChild(postDiv);
     })
 }
+
+const allPostsButton = document.querySelector('.allPosts');
+allPostsButton.addEventListener('click', () => {
+    clear(anchor);
+    fetch('http://localhost:8080/')
+        .then(response => response.json())
+        .then(response => displayAllPosts(response));
+})
+
+const clear = (chosenElement) => {
+    while(chosenElement.firstChild){
+        chosenElement.removeChild(chosenElement.firstChild);
+    }   
+}
+
+
+const displayAllHashtags = (jsonResponse) => {
+    const hashtagHeader = document.createElement('div');
+    hashtagHeader.innerText = "All Hashtags";
+    hashtagHeader.classList.add('hashtagHeader');
+    anchor.appendChild(hashtagHeader);
+
+    jsonResponse.forEach(hashtag => {
+        const hashtagsAnchor = document.createElement('div');
+        hashtagsAnchor.classList.add('hashtagsAnchor');
+
+        let hashtagContent = document.createElement('div');
+        hashtagContent.innerText = "#" + hashtag.content;
+        hashtagsAnchor.appendChild(hashtagContent);
+        hashtagContent.classList.add('hashtagContent');
+        anchor.appendChild(hashtagsAnchor);
+    })
+}
+
+const allHashtagsButton = document.querySelector('.allHashtags');
+
+allHashtagsButton.addEventListener('click', () => {
+    clear(anchor);
+    fetch('http://localhost:8080/hashtags')
+        .then(response => response.json())
+        .then(response => displayAllHashtags(response));
+})
--- src/main/java/com/example/ShrimpHeaven/Populator.java
+++ src/main/java/com/example/ShrimpHeaven/Populator.java
@@ -32,18 +32,27 @@ public class Populator implements CommandLineRunner {
         authorRepo.save(author1);
         PostCategory category1 = new PostCategory("Thoughts & Opinions");
         categoryRepository.save(category1);
-        Post post1 = new Post("The Joys Of The McElroys", author1, category1, "These boys are great!");
+        Post post1 = new Post("The Joys Of The McElroys", author1, category1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a tortor condimentum, sagittis quam id, mollis nulla.");
         postRepo.save(post1);
         Hashtag hashtag1 = new Hashtag("cool");
         hashtagRepo.save(hashtag1);
         post1.addHashtag(hashtag1);
+        Hashtag hashtag4 = new Hashtag("wow");
+        hashtagRepo.save(hashtag4);
+        post1.addHashtag(hashtag4);
+        Hashtag hashtag5 = new Hashtag("sexy");
+        hashtagRepo.save(hashtag5);
+        post1.addHashtag(hashtag5);
+        Hashtag hashtag6 = new Hashtag("fantastic");
+        hashtagRepo.save(hashtag6);
+        post1.addHashtag(hashtag6);
         postRepo.save(post1);
 
         Author author2 = new Author("Greg Gregerson");
         authorRepo.save(author2);
         PostCategory category2 = new PostCategory("Stories");
         categoryRepository.save(category2);
-        Post post2 = new Post("When I Met Clint", author2, category2, "We kissed");
+        Post post2 = new Post("When I Met Clint", author2, category2, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a tortor condimentum, sagittis quam id, mollis nulla.");
         postRepo.save(post2);
         Hashtag hashtag2 = new Hashtag("romantic");
         hashtagRepo.save(hashtag2);
@@ -54,7 +63,7 @@ public class Populator implements CommandLineRunner {
         authorRepo.save(author3);
         PostCategory category3 = new PostCategory("Questions");
         categoryRepository.save(category3);
-        Post post3 = new Post("Favorite McElroy goof?", author3, category3, "Mine is Shrimp Heaven Now!");
+        Post post3 = new Post("Favorite McElroy goof?", author3, category3, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a tortor condimentum, sagittis quam id, mollis nulla.");
         postRepo.save(post3);
         Hashtag hashtag3 = new Hashtag("amazing");
         hashtagRepo.save(hashtag3);
